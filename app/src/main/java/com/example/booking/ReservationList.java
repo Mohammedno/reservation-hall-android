cdd8fb0fb3ebade787196f4edf31ced1dbe0261d
Mohammednour <mohammednourmusta492@gmail.com>
2020-09-20 10:52:31 -0700

    hllo world

---
+++ app/src/main/java/com/example/booking/ReservationList.java
@@ -0,0 +1,14 @@
+package com.example.booking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReservationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_list);
+    }
+}
