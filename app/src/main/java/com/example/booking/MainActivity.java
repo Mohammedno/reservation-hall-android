91daf76a59f23362925af48e4ae7af91adc68a8e
Mohammednour <mohammednourmusta492@gmail.com>
2020-10-21 02:58:41 -0700

    ddd

--- app/src/main/java/com/example/booking/MainActivity.java
+++ app/src/main/java/com/example/booking/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.booking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    Button reserv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        reserv = (Button) findViewById(R.id.reserv);
+        reserv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openReservationList();
+            }
+        });
     }
+
+    private void openReservationList() {
+        Intent intent = new Intent(this, ReservationList.class);
+        startActivity(intent);
+    }
+
+
 }
+
